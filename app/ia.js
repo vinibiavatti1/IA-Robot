dbd5785411bb698c4395c9a53bb03405b1f8ed44
Vinicius Reif <vinicius.reif@senior.com.br>
2019-06-25 17:38:12 -0300

    Ajuste na rotina de busca

--- app/ia.js
+++ app/ia.js
@@ -29,7 +29,7 @@ var zoom = 20;
 /**
  * Init
  */
-$(document).ready(function() {
+$(document).ready(function () {
     criarMatriz();
     desenharTabuleiro();
 });
@@ -38,13 +38,13 @@ $(document).ready(function() {
  * Criar mapa
  */
 function novo() {
-    if(confirm("Deseja iniciar uma nova simulação?")) {
+    if (confirm("Deseja iniciar uma nova simulação?")) {
         _largura = prompt("informe a largura do mapa (min: 5, max: 200):");
         _altura = prompt("informe a altura do mapa (min: 5, max: 200):");
-        if(isNaN(_largura) || parseInt(_largura) < 5 || parseInt(_largura) > 200) {
+        if (isNaN(_largura) || parseInt(_largura) < 5 || parseInt(_largura) > 200) {
             _largura = 20;
         }
-        if(isNaN(_altura)  || parseInt(_altura) < 5 || parseInt(_altura) > 200) {
+        if (isNaN(_altura) || parseInt(_altura) < 5 || parseInt(_altura) > 200) {
             _altura = 10;
         }
         largura = _largura;
@@ -59,25 +59,25 @@ function novo() {
  * Iniciar simulação
  */
 function iniciarSimulacao() {
-    if(!existeObjeto(objetos.robo)) {
+    if (!existeObjeto(objetos.robo)) {
         alert("Para iniciar a simulação, adicione o robô no tabuleiro.");
         return;
     }
-    if(!existeObjeto(objetos.alvo)) {
+    if (!existeObjeto(objetos.alvo)) {
         alert("Para iniciar a simulação, adicione um alvo no tabuleiro.");
         return;
     }
     console.log("Simulação iniciada.");
     posicao = busca();
-    if(!posicao) {
+    if (!posicao) {
         alert("Não foram encontrados caminhos possíveis até o alvo.");
         return;
     }
     simulacao = true;
     solucao = [];
     var primeiro = true;
-    while(posicao != null) {
-        if(posicao.pai != null && !primeiro) {
+    while (posicao != null) {
+        if (posicao.pai != null && !primeiro) {
             matriz[posicao.y][posicao.x] = objetos.caminho;
         }
         primeiro = false;
@@ -88,8 +88,8 @@ function iniciarSimulacao() {
     solucao = solucao.reverse();
     var i = 0;
     desenharTabuleiro();
-    var intervalo = setInterval(function() {
-        if(!solucao[i]) {
+    var intervalo = setInterval(function () {
+        if (!solucao[i]) {
             clearInterval(intervalo);
             console.log("Simulação finalizada.");
             simulacao = false;
@@ -111,24 +111,30 @@ function iniciarSimulacao() {
  */
 function busca() {
     var _matriz = [];
-    for(var i = 0; i < matriz.length; i++) {
+    for (var i = 0; i < matriz.length; i++) {
         _matriz[i] = matriz[i].slice(0);
     }
-    
+
     pilha = [{
         x: robo.x,
         y: robo.y,
         pai: null
     }];
-    
-    while(pilha.length > 0) {
+
+    while (pilha.length > 0) {
         var posicao = pilha.shift();
-        if(_matriz[posicao.y][posicao.x] == objetos.alvo) {
-            return posicao;
-        }
+        console.log(posicao.x + " - " + posicao.y);
 
         var objeto = _matriz[posicao.y][posicao.x - 1];
-        if(objeto != objetos.parede) {
+        if (objeto != objetos.parede) {
+            if (_matriz[posicao.y][posicao.x - 1] == objetos.alvo) {
+                return {
+                    x: posicao.x - 1,
+                    y: posicao.y,
+                    pai: posicao
+                };
+            }
+            _matriz[posicao.y][posicao.x - 1] = objetos.parede;
             pilha.push({
                 x: posicao.x - 1,
                 y: posicao.y,
@@ -136,8 +142,16 @@ function busca() {
             });
         }
 
-        objeto = _matriz[posicao.y -1][posicao.x]
-        if(objeto != objetos.parede) {
+        objeto = _matriz[posicao.y - 1][posicao.x]
+        if (objeto != objetos.parede) {
+            if (_matriz[posicao.y - 1][posicao.x] == objetos.alvo) {
+                return {
+                    x: posicao.x,
+                    y: posicao.y - 1,
+                    pai: posicao
+                };
+            }
+            _matriz[posicao.y - 1][posicao.x] = objetos.parede;
             pilha.push({
                 x: posicao.x,
                 y: posicao.y - 1,
@@ -146,7 +160,15 @@ function busca() {
         }
 
         objeto = _matriz[posicao.y][posicao.x + 1]
-        if(objeto != objetos.parede) {
+        if (objeto != objetos.parede) {
+            if (_matriz[posicao.y][posicao.x + 1] == objetos.alvo) {
+                return {
+                    x: posicao.x + 1,
+                    y: posicao.y,
+                    pai: posicao
+                };
+            }
+            _matriz[posicao.y][posicao.x + 1] = objetos.parede;
             pilha.push({
                 x: posicao.x + 1,
                 y: posicao.y,
@@ -155,7 +177,15 @@ function busca() {
         }
 
         objeto = _matriz[posicao.y + 1][posicao.x]
-        if(objeto != objetos.parede) {
+        if (objeto != objetos.parede) {
+            if (_matriz[posicao.y + 1][posicao.x] == objetos.alvo) {
+                return {
+                    x: posicao.x,
+                    y: posicao.y + 1,
+                    pai: posicao
+                };
+            }
+            _matriz[posicao.y + 1][posicao.x] = objetos.parede;
             pilha.push({
                 x: posicao.x,
                 y: posicao.y + 1,
@@ -163,9 +193,17 @@ function busca() {
             });
         }
 
-        if(diagonal) {
+        if (diagonal) {
             objeto = _matriz[posicao.y - 1][posicao.x - 1];
-            if(objeto != objetos.parede) {
+            if (objeto != objetos.parede) {
+                if (_matriz[posicao.y - 1][posicao.x - 1] == objetos.alvo) {
+                    return {
+                        x: posicao.x - 1,
+                        y: posicao.y - 1,
+                        pai: posicao
+                    };
+                }
+                _matriz[posicao.y - 1][posicao.x - 1] = objetos.parede;
                 pilha.push({
                     x: posicao.x - 1,
                     y: posicao.y - 1,
@@ -174,7 +212,15 @@ function busca() {
             }
 
             objeto = _matriz[posicao.y - 1][posicao.x + 1]
-            if(objeto != objetos.parede) {
+            if (objeto != objetos.parede) {
+                if (_matriz[posicao.y - 1][posicao.x + 1] == objetos.alvo) {
+                    return {
+                        x: posicao.x + 1,
+                        y: posicao.y - 1,
+                        pai: posicao
+                    };
+                }
+                _matriz[posicao.y - 1][posicao.x + 1] = objetos.parede;
                 pilha.push({
                     x: posicao.x + 1,
                     y: posicao.y - 1,
@@ -183,7 +229,15 @@ function busca() {
             }
 
             objeto = _matriz[posicao.y + 1][posicao.x - 1]
-            if(objeto != objetos.parede) {
+            if (objeto != objetos.parede) {
+                if (_matriz[posicao.y + 1][posicao.x - 1] == objetos.alvo) {
+                    return {
+                        x: posicao.x - 1,
+                        y: posicao.y + 1,
+                        pai: posicao
+                    };
+                }
+                _matriz[posicao.y + 1][posicao.x - 1] = objetos.parede;
                 pilha.push({
                     x: posicao.x - 1,
                     y: posicao.y + 1,
@@ -192,7 +246,15 @@ function busca() {
             }
 
             objeto = _matriz[posicao.y + 1][posicao.x + 1]
-            if(objeto != objetos.parede) {
+            if (objeto != objetos.parede) {
+                if (_matriz[posicao.y + 1][posicao.x + 1] == objetos.alvo) {
+                    return {
+                        x: posicao.x + 1,
+                        y: posicao.y + 1,
+                        pai: posicao
+                    };
+                }
+                _matriz[posicao.y + 1][posicao.x + 1] = objetos.parede;
                 pilha.push({
                     x: posicao.x + 1,
                     y: posicao.y + 1,
@@ -200,8 +262,6 @@ function busca() {
                 });
             }
         }
-
-        _matriz[posicao.y][posicao.x] = objetos.parede;
     }
     return false;
 }
@@ -210,10 +270,10 @@ function busca() {
  * Criar matriz de mapa
  */
 function criarMatriz() {
-    for(var i = 0; i < altura; i++) {
+    for (var i = 0; i < altura; i++) {
         matriz[i] = [];
-        for(var j = 0; j < largura; j++) {
-            if(i == 0 || j == 0 || j == largura - 1 || i == altura - 1) {
+        for (var j = 0; j < largura; j++) {
+            if (i == 0 || j == 0 || j == largura - 1 || i == altura - 1) {
                 matriz[i][j] = objetos.parede;
             } else {
                 matriz[i][j] = objetos.nada;
@@ -231,10 +291,10 @@ function desenharTabuleiro() {
     $("#tabuleiro tbody").empty();
     var estilo = `style='padding: ${zoom}px'`;
     var html = "";
-    for(var i = 0; i < altura; i++) {
+    for (var i = 0; i < altura; i++) {
         html += "<tr>";
-        for(var j = 0; j < largura; j++) {
-            if(matriz[i][j] == objetos.parede) {
+        for (var j = 0; j < largura; j++) {
+            if (matriz[i][j] == objetos.parede) {
                 html += `<td class='quadrante parede' ${estilo} onclick='selecionarQuadrante(${j},${i})'></td>`;
             } else if (matriz[i][j] == objetos.nada) {
                 html += `<td class='quadrante' ${estilo} onclick='selecionarQuadrante(${j},${i})'></td>`;
@@ -256,22 +316,22 @@ function desenharTabuleiro() {
  * @param {*} _ferramenta 
  */
 function selecionarFerramenta(_ferramenta) {
-    if(simulacao) return;
+    if (simulacao) return;
     ferramenta = _ferramenta;
     $("#botao-ponteiro").removeClass("botao-selecionado");
     $("#botao-alvo").removeClass("botao-selecionado");
     $("#botao-robo").removeClass("botao-selecionado");
     $("#botao-parede").removeClass("botao-selecionado");
-    if(_ferramenta == ferramentas.ponteiro) {
+    if (_ferramenta == ferramentas.ponteiro) {
         $("#botao-ponteiro").addClass("botao-selecionado");
     }
-    if(_ferramenta == ferramentas.alvo) {
+    if (_ferramenta == ferramentas.alvo) {
         $("#botao-alvo").addClass("botao-selecionado");
     }
-    if(_ferramenta == ferramentas.robo) {
+    if (_ferramenta == ferramentas.robo) {
         $("#botao-robo").addClass("botao-selecionado");
     }
-    if(_ferramenta == ferramentas.parede) {
+    if (_ferramenta == ferramentas.parede) {
         $("#botao-parede").addClass("botao-selecionado");
     }
 }
@@ -282,22 +342,22 @@ function selecionarFerramenta(_ferramenta) {
  * @param {*} i 
  */
 function selecionarQuadrante(j, i) {
-    if(simulacao) return;
-    if(i == 0 || j == 0 || j == largura - 1 || i == altura - 1) {
+    if (simulacao) return;
+    if (i == 0 || j == 0 || j == largura - 1 || i == altura - 1) {
         return;
     }
-    if(ferramenta == ferramentas.parede) {
-        if(matriz[i][j] == objetos.nada) {
+    if (ferramenta == ferramentas.parede) {
+        if (matriz[i][j] == objetos.nada) {
             matriz[i][j] = objetos.parede;
-        } else if(matriz[i][j] == objetos.parede) {
+        } else if (matriz[i][j] == objetos.parede) {
             matriz[i][j] = objetos.nada;
         }
         console.log(matriz);
     }
-    if(ferramenta == ferramentas.robo) {
-        if(matriz[i][j] == objetos.robo) {
+    if (ferramenta == ferramentas.robo) {
+        if (matriz[i][j] == objetos.robo) {
             matriz[i][j] = objetos.nada;
-        } else if(matriz[i][j] == objetos.nada) {
+        } else if (matriz[i][j] == objetos.nada) {
             removerObjetos(objetos.robo);
             matriz[i][j] = objetos.robo;
             robo = {
@@ -306,10 +366,10 @@ function selecionarQuadrante(j, i) {
             }
         }
     }
-    if(ferramenta == ferramentas.alvo) {
-        if(matriz[i][j] == objetos.alvo) {
+    if (ferramenta == ferramentas.alvo) {
+        if (matriz[i][j] == objetos.alvo) {
             matriz[i][j] = objetos.nada;
-        } else if(matriz[i][j] == objetos.nada) {
+        } else if (matriz[i][j] == objetos.nada) {
             removerObjetos(objetos.alvo);
             matriz[i][j] = objetos.alvo;
         }
@@ -322,9 +382,9 @@ function selecionarQuadrante(j, i) {
  * @param {*} objeto 
  */
 function removerObjetos(objeto) {
-    for(var i = 0; i < altura; i++) {
-        for(var j = 0; j < largura; j++) {
-            if(matriz[i][j] == objeto) {
+    for (var i = 0; i < altura; i++) {
+        for (var j = 0; j < largura; j++) {
+            if (matriz[i][j] == objeto) {
                 matriz[i][j] = objetos.nada;
             }
         }
@@ -336,9 +396,9 @@ function removerObjetos(objeto) {
  * @param {*} objeto 
  */
 function existeObjeto(objeto) {
-    for(var i = 0; i < altura; i++) {
-        for(var j = 0; j < largura; j++) {
-            if(matriz[i][j] == objeto) {
+    for (var i = 0; i < altura; i++) {
+        for (var j = 0; j < largura; j++) {
+            if (matriz[i][j] == objeto) {
                 return true;
             }
         }
@@ -360,20 +420,20 @@ function getObjeto(x, y) {
  * @param {*} id 
  */
 function selecionarExemplo(id) {
-    if(!confirm("Deseja abrir este exemplo?")) {
+    if (!confirm("Deseja abrir este exemplo?")) {
         return;
     }
-    if(id == 1) {
+    if (id == 1) {
         matriz = [
-            [1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,0,0,1,0,0,0,1,0,0,0,1],
-            [1,0,2,1,0,1,0,1,0,1,0,1],
-            [1,0,1,1,0,1,0,0,0,0,0,1],
-            [1,0,0,0,0,1,0,0,0,1,0,1],
-            [1,0,1,1,1,1,1,1,1,1,0,1],
-            [1,0,1,3,1,0,0,0,0,0,0,1],
-            [1,0,1,0,0,0,0,1,0,0,0,1],
-            [1,1,1,1,1,1,1,1,1,1,1,1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+            [1, 0, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
         altura = matriz.length;
         largura = matriz[0].length;
@@ -384,22 +444,22 @@ function selecionarExemplo(id) {
         desenharTabuleiro();
         selecionarFerramenta(ferramentas.ponteiro);
     }
-    if(id == 2) {
+    if (id == 2) {
         matriz = [
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,2,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
         altura = matriz.length;
         largura = matriz[0].length;
@@ -410,22 +470,22 @@ function selecionarExemplo(id) {
         desenharTabuleiro();
         selecionarFerramenta(ferramentas.ponteiro);
     }
-    if(id == 3) {
+    if (id == 3) {
         matriz = [
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,1,0,1,1,1,0,1,0,1,0,1,3,1],
-            [1,0,1,0,1,1,1,0,1,0,1,0,1,1],
-            [1,1,0,1,0,1,1,1,0,1,1,1,0,1],
-            [1,0,1,1,1,0,1,1,1,0,1,0,1,1],
-            [1,1,0,1,1,1,0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,1,1,0,1,1,1,0,1,1],
-            [1,1,0,1,0,1,1,1,0,1,1,1,0,1],
-            [1,0,1,0,1,0,1,1,1,0,1,1,1,1],
-            [1,1,0,1,0,1,0,1,1,1,0,1,1,1],
-            [1,0,1,0,1,0,1,0,1,1,1,0,1,1],
-            [1,1,0,1,0,1,0,1,0,1,0,1,0,1],
-            [1,2,1,0,1,0,1,0,1,0,1,0,1,1],
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 3, 1],
+            [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+            [1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1],
+            [1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+            [1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+            [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+            [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
         altura = matriz.length;
         largura = matriz[0].length;
@@ -445,7 +505,7 @@ function permiteDiagonal() {
     diagonal = !diagonal;
     $("#botao-diagonal").removeClass("botao-on");
     $("#botao-diagonal").removeClass("botao-off");
-    if(diagonal) {
+    if (diagonal) {
         $("#botao-diagonal").addClass("botao-on");
         $("#botao-diagonal").html("Diagonal (ON)");
     } else {
@@ -456,7 +516,7 @@ function permiteDiagonal() {
 
 function zoomin() {
     zoom += 5;
-    if(zoom > 30) {
+    if (zoom > 30) {
         zoom = 30;
     }
     desenharTabuleiro();
@@ -464,7 +524,7 @@ function zoomin() {
 
 function zoomout() {
     zoom -= 5;
-    if(zoom < 5) {
+    if (zoom < 5) {
         zoom = 5;
     }
     desenharTabuleiro();
